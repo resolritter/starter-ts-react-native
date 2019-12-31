5b4b5a76b714c3a74ec151680ec4989640433762
resolritter <joao.paulo.silvasouza@hotmail.com>
2019-12-31 09:41:30 -0300

    fix: consistent filename casing

--- e2e/__tests__/App.spec.ts
+++ e2e/__tests__/App.spec.ts
@@ -1,7 +1,7 @@
 import Labels from "../../src/Labels"
 
 describe("Create Android session (wd)", () => {
-  it("Captures the text in the home page", async () => {
+  it("Captures the sample text in the Home page", async () => {
     const wholeApp = await browser.$(`~${Labels.Root}`)
     await wholeApp.waitForDisplayed(3000)
 
